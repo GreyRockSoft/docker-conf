bfb5559e5283c678818e4ffe75bf5de3ad7dffce
Ryan Moore <ryan.moore@greyrocksoft.com>
2014-09-18 13:07:02 -0600

    Added  a comment explaining the methodoligy for performing the replacement.

--- template/file.go
+++ template/file.go
@@ -5,7 +5,10 @@ import (
 )
 
 func ProcessTemplate(file Template) error {
-	//assume that file exists and is a tmpl file
+	// assume that file exists and is a tmpl file
 	fmt.Printf("Processing env replacement for source file (%s) to target file (%s)\n", file.Name, file.TargetFile)
+
+	// open the first file.  Scan it a line at a time.  Replace the value with the environment variable.  Write to target file.
+
 	return nil
 }
