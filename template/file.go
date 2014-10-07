8457483e32dd9ebd43bedba0118a7196ec9d98c8
Ryan Moore <ryan.moore@greyrocksoft.com>
2014-10-07 17:20:44 -0600

    Starting to work through some of the template processing code.  Need to parse the path now and set it up such that it will place the files in the correct spot after getting generated.

--- template/file.go
+++ template/file.go
@@ -2,13 +2,61 @@ package template
 
 import (
 	"fmt"
+	"os"
+	tmpl "text/template"
 )
+
+var (
+	funcMap map[string]interface{}
+)
+
+func init() {
+	funcMap := make(map[string]interface{})
+	funcMap["env"] = getEnv
+}
+
+func getEnv(envname string) (string, error) {
+	env := os.Getenv(envname)
+	if env == "" {
+		return "", fmt.Errorf("Could not find the enviornment variable %s", envname)
+	}
+	return env, nil
+}
 
 func ProcessTemplate(file Template) error {
 	// assume that file exists and is a tmpl file
 	fmt.Printf("Processing env replacement for source file (%s) to target file (%s)\n", file.Name, file.TargetFile)
+	confTmpl, err := tmpl.ParseFiles(file.Name)
 
-	// open the first file.  Scan it a line at a time.  Replace the value with the environment variable.  Write to target file.
+	if err != nil {
+		return err
+	}
+
+	confTmpl.Funcs(funcMap)
+
+	err = os.MkdirAll(file.TargetFile, 0755)
+
+	if err != nil {
+		return err
+	}
+
+	outfile, err := os.Create(file.TargetFile)
+
+	if err != nil {
+		return err
+	}
+
+	err = confTmpl.Execute(outfile, nil)
+
+	if err != nil {
+		return err
+	}
+
+	err = outfile.Close()
+
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
